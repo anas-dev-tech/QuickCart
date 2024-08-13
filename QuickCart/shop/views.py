0c216887dfa7db52cf1d3498333e9622d1105f2d
Anas Al-Ward <anasalwardtech@gmail.com>
2024-08-14 01:03:03 +0300

    create product catalog

--- QuickCart/shop/views.py
+++ QuickCart/shop/views.py
@@ -1,3 +1,33 @@
 from django.shortcuts import render
+from django.views.generic import ListView, DetailView
+from .models import Product, Category
+from django.shortcuts import get_object_or_404
 
-# Create your views here.
+class ProductListMixin(object):
+    model = Product
+    template_name = 'shop/product/list.html'
+    context_object_name = 'products'
+
+    def get_queryset(self):
+        return Product.objects.filter(available=True)
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['categories'] = Category.objects.all()
+        return context
+
+
+class ProductListView(ProductListMixin, ListView):
+    pass
+
+class ProductListByCategoryView(ProductListMixin, ListView):
+    def get_queryset(self):
+        category = get_object_or_404(Category, slug=self.kwargs['category_slug'])
+        return Product.objects.filter(category=category, available=True)
+
+
+class ProductDetailView(DetailView):
+    model = Product
+    template_name = 'shop/product/detail.html'
+    context_object_name = 'product'
+    def get_object(self, queryset=None):
+        return get_object_or_404(Product, id=self.kwargs['id'], slug=self.kwargs['slug'], available=True)
