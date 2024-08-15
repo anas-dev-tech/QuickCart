45d6a0eab2dc208631dfc6f56b04ca437795beb5
Anas Al-Ward <anasalwardtech@gmail.com>
2024-08-15 17:14:15 +0300

    config celery & rabbitmq to send order_created emails

--- QuickCart/orders/views.py
+++ QuickCart/orders/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from .models import OrderItem
 from .forms import OrderCreateForm
 from cart.cart import Cart
-
+from .tasks import order_created
 
 
 def order_create(request):
@@ -20,11 +20,8 @@ def order_create(request):
                 )
                 
             cart.clear()
-            return render(
-                request,
-                'orders/order/created.html',
-                {'order': order}
-            )
+            order_created.delay(order.id)
+            return render(request, 'orders/order/created.html', {'order': order})
     else: # GET
         form = OrderCreateForm()
         return render(
